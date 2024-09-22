721b33d7e9536c7b7806c1b14f0d2f0d6921c6ed
tomas <tomascostapp@gmail.com>
2024-09-22 16:22:14 -0300

    Coorecciones

--- src/app/components/faqs.tsx
+++ src/app/components/faqs.tsx
@@ -46,7 +46,7 @@ export default function Faqs() {
 								className="w-full cursor-pointer bg-primary p-[20px] rounded-[6px] border border-white/50 mb-[10px]"
 							>
 								<h3 className="--sora text-[16px] md:text-[18px] text-white lg:text-[20px] flex justify-between items-center">{item.pregunta}<svg className={`${open === item.pregunta ? 'rotate-45' : ''} min-w-[20px] size-[20px] transition-all duration-300`} fill="#FFF" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg></h3>
-								<div className={`overflow-hidden duration-500 transition-all ${open === item.pregunta  ? 'max-h-[2000px]' : 'max-h-0'}`}>
+								<div className={`normal-case overflow-hidden duration-500 transition-all ${open === item.pregunta  ? 'max-h-[2000px]' : 'max-h-0'}`}>
 									<p className="text-white/80 text-[16px] pt-[10px]">{item.respuesta}</p>
 								</div>
 							</div>
