d7b4b65eeecb758688d3fba6d1a907c923cd2d0a
tomas <tomascostapp@gmail.com>
2024-09-22 18:13:46 -0300

    Coorecciones

--- src/app/components/svgs/star.tsx
+++ src/app/components/svgs/star.tsx
@@ -1,7 +1,7 @@
 export default function Star() {
 	return (
 		<svg width="30" height="29" viewBox="0 0 30 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path fill-rule="evenodd" clip-rule="evenodd" d="M14.9194 0L19.0258 9.98134L29.8387 10.7813L21.5639 17.7501L24.14 28.2258L14.9194 22.5516L5.69869 28.2258L8.27509 17.7501L0 10.7813L10.8129 9.98134L14.9194 0Z" fill="#ECE8FF"/>
+			<path fill-rule="evenodd" clip-rule="evenodd" d="M14.9194 0L19.0258 9.98134L29.8387 10.7813L21.5639 17.7501L24.14 28.2258L14.9194 22.5516L5.69869 28.2258L8.27509 17.7501L0 10.7813L10.8129 9.98134L14.9194 0Z" fill="#FFC907"/>
 		</svg>
 	)
 }
