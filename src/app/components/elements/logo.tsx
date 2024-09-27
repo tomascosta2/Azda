03cef3d95fa52b61a13568bf69ae47b297970f8d
tomas <tomascostapp@gmail.com>
2024-09-27 13:14:46 -0300

    Abajo

--- src/app/components/elements/logo.tsx
+++ src/app/components/elements/logo.tsx
@@ -1,6 +1,7 @@
 export default function Logo() {
 	return (
 		<h2 className="w-[220px] lg:w-[250px]">
+			<img className="hidden" src="/images/logo-azda-share.jpg" alt="Azda Consulting" />
 			<img src="/images/logo-azda.png" alt="Azda Consulting" />
 		</h2>
 	)
