421482d7bb2dcd984c47ce9db409468d3b27e175
tomas <tomascostapp@gmail.com>
2024-09-28 11:53:09 -0300

    Detalle

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,15 +3,17 @@ import Logo from "./elements/logo";
 
 export default function Header() {
 	return (
-		<header className="relative z-40 max-w-[1200px] mx-auto px-4 pt-[5px] tcp-header">
-			<div className="flex justify-center lg:justify-between items-center">
-				<Logo/>
-				<nav className="hidden lg:flex gap-[30px]">
-					<Link className="text-[16px] font-medium text-[#F5F5F5]/80 hover:text-white transition-all" href="#testimonios">Testimonios</Link>
-					<Link className="text-[16px] font-medium text-[#F5F5F5]/80 hover:text-white transition-all" href="#nosotros">Quienes Somos</Link>
-					<Link className="text-[16px] font-medium text-[#F5F5F5]/80 hover:text-white transition-all" href="#proceso">Estrategia</Link>
-					<Link className="text-[16px] font-medium text-[#F5F5F5]/80 hover:text-white transition-all" href="#faqs">Preguntas</Link>
-				</nav>
+		<header className="relative z-40 px-4 pt-[5px] tcp-header">
+			<div className="tc-container">
+				<div className="flex justify-center lg:justify-between items-center">
+					<Logo/>
+					<nav className="hidden lg:flex gap-[30px]">
+						<Link className="text-[16px] font-medium text-[#F5F5F5]/80 hover:text-white transition-all" href="#testimonios">Testimonios</Link>
+						<Link className="text-[16px] font-medium text-[#F5F5F5]/80 hover:text-white transition-all" href="#nosotros">Quienes Somos</Link>
+						<Link className="text-[16px] font-medium text-[#F5F5F5]/80 hover:text-white transition-all" href="#proceso">Estrategia</Link>
+						<Link className="text-[16px] font-medium text-[#F5F5F5]/80 hover:text-white transition-all" href="#faqs">Preguntas</Link>
+					</nav>
+				</div>
 			</div>
 		</header>
 	)
--- src/app/components/heroTest.tsx
+++ src/app/components/heroTest.tsx
@@ -10,9 +10,9 @@ export default function HeroSectionA() {
 			<div className="h-full py-[80px] overflow-hidden">
 				<img className="absolute top-0 left-0 w-full h-full object-cover" src="/images/azda-banner-bg.webp" alt="Azda" />
 				<section className="relative z-40 max-w-[1200px] mx-auto px-4">
-					<span className="block mx-auto --sora text-[18px] lg:text-[19px] font-medium text-center text-white/90">¿que tu facturación mensual sea variable no te deja estar tranquilo?</span>
+					<span className="block mx-auto --sora text-[16px] md:text-[19px] font-medium text-center text-white/90">¿que tu facturación mensual sea variable no te deja estar tranquilo?</span>
 					<h1 className="text-center text-[29px] md:text-[58px] tracking-tight font-extrabold leading-[115%] max-w-[1100px] mx-auto my-[25px]">Escalamos tu negocio de coaching / consultoría de manera predecible y sostenible en el tiempo.</h1>
-					<p className="text-center max-w-[760px] mx-auto text-[18px] lg:text-[19px] font-medium text-white/80">Creamos Sistemas validados y metodologías probadas para llegar a <strong className="text-white/90">clientes premium en todo el mundo</strong> a través de las redes sociales.</p>
+					<p className="text-center max-w-[760px] mx-auto text-[16px] md:text-[19px] font-medium text-white/80">Creamos Sistemas validados y metodologías probadas para llegar a <strong className="text-white/90">clientes premium en todo el mundo</strong> a través de las redes sociales.</p>
 					<Cta/>
 				</section>
 			</div>
