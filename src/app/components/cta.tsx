9cee20a3345811bd5f2f4a9061b0779c29cdfcbc
tomas <tomascostapp@gmail.com>
2025-03-10 11:20:04 -0300

    Detalle

--- src/app/components/cta.tsx
+++ src/app/components/cta.tsx
@@ -2,7 +2,7 @@ export default function Cta() {
 	return (
 		<div className="lg:w-fit mx-auto">
 			<a 
-				className="drop-shadow-[0_35px_35px_#FFFFFF35] block text-center w-full lg:w-fit mb-[12px] mt-[40px] mx-auto py-[24px] lg:px-[70px] text-[18px] lg:text-[20px] font-bold rounded-[4px] bg-[#ece9fb] text-[#0F0248]"
+				className="drop-shadow-[0_35px_35px_#FFFFFF35] block text-center w-full lg:w-fit mb-[12px] mt-[40px] mx-auto py-[24px] lg:px-[70px] text-[18px] lg:text-[20px] font-bold rounded-[4px] bg-[#FF9721] text-[#FFF]"
 				href="#vsl"
 			>
 				¡Agenda Tu Consultoria Gratuita!
